fa4bbacc3e85125c60dff3d5f9a6dc6d8efbdccb
ssMMiles <github@milesmoonlove.com>
2022-04-04 12:39:17 +0100

    chore(tests): basic tests for command handling

---
+++ tests/data.ts
@@ -0,0 +1,71 @@
+const testCommandInteraction = {
+  application_id: "820553335492837377",
+  channel_id: "690256246003204228",
+  data: { id: "952134723654799410", name: "test", type: 1 },
+  guild_id: "690255418060046348",
+  guild_locale: "en-US",
+  id: "959430816138661908",
+  locale: "en-GB",
+  member: {
+    avatar: "66ccf96f0b4230525f0407a479e43440",
+    communication_disabled_until: null,
+    deaf: false,
+    flags: 0,
+    is_pending: false,
+    joined_at: "2020-03-19T17:48:36.147000+00:00",
+    mute: false,
+    nick: "goblin giggler",
+    pending: false,
+    permissions: "4398046511103",
+    premium_since: "2021-06-11T19:17:43.822000+00:00",
+    roles: ["690258902016852041", "693450982423789591"],
+    user: {
+      avatar: "ced436c7745589b6357303089f0fabc9",
+      discriminator: "6969",
+      id: "170531778354741248",
+      public_flags: 131648,
+      username: "miles"
+    }
+  },
+  token:
+    "aW50ZXJhY3Rpb246OTU5NDMwODE2MTM4NjYxOTA4OnpDejNWbmt6VUJuNjdWcVpCeWdyM2pDRENlNlpnVExqVU5rekI2Tmp0MFc4RVEwaVQ3WE9kOVlLRFFuWkgzWlJHNXZiWGNPOTFBcmt5Yjk2VE15Nk1jRVZTRkRVR3FpeVpJVU93TnJlc2xDeTNkVmNYYTExcHF5b3FNdjBnRUk0",
+  type: 2,
+  version: 1
+};
+
+const test2CommandInteraction = {
+  application_id: "820553335492837377",
+  channel_id: "690256246003204228",
+  data: { id: "952134723654799410", name: "test2", type: 1 },
+  guild_id: "690255418060046348",
+  guild_locale: "en-US",
+  id: "959430816138661908",
+  locale: "en-GB",
+  member: {
+    avatar: "66ccf96f0b4230525f0407a479e43440",
+    communication_disabled_until: null,
+    deaf: false,
+    flags: 0,
+    is_pending: false,
+    joined_at: "2020-03-19T17:48:36.147000+00:00",
+    mute: false,
+    nick: "goblin giggler",
+    pending: false,
+    permissions: "4398046511103",
+    premium_since: "2021-06-11T19:17:43.822000+00:00",
+    roles: ["690258902016852041", "693450982423789591"],
+    user: {
+      avatar: "ced436c7745589b6357303089f0fabc9",
+      discriminator: "6969",
+      id: "170531778354741248",
+      public_flags: 131648,
+      username: "miles"
+    }
+  },
+  token:
+    "aW50ZXJhY3Rpb246OTU5NDMwODE2MTM4NjYxOTA4OnpDejNWbmt6VUJuNjdWcVpCeWdyM2pDRENlNlpnVExqVU5rekI2Tmp0MFc4RVEwaVQ3WE9kOVlLRFFuWkgzWlJHNXZiWGNPOTFBcmt5Yjk2VE15Nk1jRVZTRkRVR3FpeVpJVU93TnJlc2xDeTNkVmNYYTExcHF5b3FNdjBnRUk0",
+  type: 2,
+  version: 1
+};
+
+export { testCommandInteraction, test2CommandInteraction };
--- tests/index.ts
+++ tests/index.ts
@@ -1,16 +1,18 @@
 import { should, use } from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { ButtonStyle, InteractionResponseType } from "discord-api-types/v10";
+import { ButtonStyle, ComponentType, InteractionResponseType } from "discord-api-types/v10";
 import "dotenv/config";
 import {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonContext,
   DiscordApplication,
+  LoadedSlashCommand,
   MessageBuilder,
   SlashCommandBuilder,
   SlashCommandContext
 } from "../src";
+import { test2CommandInteraction, testCommandInteraction } from "./data";
 
 use(chaiAsPromised);
 should();
@@ -24,41 +26,6 @@ if (
   process.exit(1);
 }
 
-const testCommandInteraction = {
-  application_id: "820553335492837377",
-  channel_id: "690256246003204228",
-  data: { id: "952134723654799410", name: "queue", type: 1 },
-  guild_id: "690255418060046348",
-  guild_locale: "en-US",
-  id: "959430816138661908",
-  locale: "en-GB",
-  member: {
-    avatar: "66ccf96f0b4230525f0407a479e43440",
-    communication_disabled_until: null,
-    deaf: false,
-    flags: 0,
-    is_pending: false,
-    joined_at: "2020-03-19T17:48:36.147000+00:00",
-    mute: false,
-    nick: "goblin giggler",
-    pending: false,
-    permissions: "4398046511103",
-    premium_since: "2021-06-11T19:17:43.822000+00:00",
-    roles: ["690258902016852041", "693450982423789591"],
-    user: {
-      avatar: "ced436c7745589b6357303089f0fabc9",
-      discriminator: "6969",
-      id: "170531778354741248",
-      public_flags: 131648,
-      username: "miles"
-    }
-  },
-  token:
-    "aW50ZXJhY3Rpb246OTU5NDMwODE2MTM4NjYxOTA4OnpDejNWbmt6VUJuNjdWcVpCeWdyM2pDRENlNlpnVExqVU5rekI2Tmp0MFc4RVEwaVQ3WE9kOVlLRFFuWkgzWlJHNXZiWGNPOTFBcmt5Yjk2VE15Nk1jRVZTRkRVR3FpeVpJVU93TnJlc2xDeTNkVmNYYTExcHF5b3FNdjBnRUk0",
-  type: 2,
-  version: 1
-};
-
 const options = {
   clientId: process.env.CLIENT_ID,
   token: process.env.TOKEN,
@@ -66,50 +33,101 @@ const options = {
 };
 
 describe("Discord Application", () => {
+  describe("Authentication", () => {
+    const publicKey = Buffer.from("67d3b5eaf0c0bf6b5a602d359daecc86a7a74053490ec37ae08e71360587c870", "hex");
+
+    const signature =
+        "dbd6c2a5df02e85cdd1779f7ff3ee14d51e56c7f7d9b30d12253ccdd9e84949327db7e1b41e636975b16103316a144f8b98a290112b19e8148bbafa6f4e04209",
+      invalidSignature = signature.replace("1", "2"),
+      timestamp = "",
+      body = "Oh no!";
+    it("Valid Signature Is Accepted", () => {
+      DiscordApplication.verifyInteractionSignature(publicKey, timestamp, signature, body).should.be.true;
+    });
+
+    it("Invalid Signature Is Denied", () => {
+      DiscordApplication.verifyInteractionSignature(publicKey, timestamp, invalidSignature, body).should.be.false;
+    });
+  });
+
   const app = new DiscordApplication(options);
 
-  it("Loading Command", () => {
-    app.commands.loadCommand(
-      new SlashCommandBuilder()
-        .setName("test")
-        .setDescription("Test command")
-        .setHandler(async (context: SlashCommandContext) => {
-          await context.reply(
-            new MessageBuilder()
-              .setContent("Test command executed!")
-              .addActionRow(
-                new ActionRowBuilder().addComponents(context.manager.components.createInstance("testButton"))
-              )
-          );
-        })
-    );
+  describe("Handling Commands", () => {
+    it("Basic Command", async () => {
+      await app.commands.load([
+        new SlashCommandBuilder()
+          .setName("test")
+          .setDescription("Test command")
+          .setHandler(async (context: SlashCommandContext) => {
+            await context.reply(new MessageBuilder().setContent("Test command executed!"));
+          })
+      ]);
 
-    app.commands.has("test").should.be.true;
-    app.commands.get("test")?.should.be.instanceOf(SlashCommandBuilder);
-  });
+      app.commands.has("test").should.be.true;
+      app.commands.get("test")?.should.be.instanceOf(LoadedSlashCommand);
 
-  it("Loading Component", () => {
-    app.components.loadComponent(
-      new ButtonBuilder("testButton")
-        .setLabel("Test Button")
-        .setStyle(ButtonStyle.Primary)
-        .setHandler(async (context: ButtonContext) => {
-          context.reply(new MessageBuilder().setContent("Test button executed!"));
-        })
-    );
+      const response = await app.handleInteraction(JSON.stringify(testCommandInteraction), false);
 
-    app.components.has("testButton").should.be.true;
-    app.components.get("testButton")?.should.be.instanceOf(ButtonBuilder);
-  });
+      response.should.deep.equal({
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          content: "Test command executed!"
+        }
+      });
+    });
 
-  it("Setting Global Commands", () => {
-    app.putGlobalCommands().should.eventually.not.throw();
-  });
+    it("Basic Command With A Button", async () => {
+      app.components.load([
+        new ButtonBuilder("testButton")
+          .setLabel("Test Button")
+          .setStyle(ButtonStyle.Primary)
+          .setHandler(async (context: ButtonContext) => {
+            context.reply(new MessageBuilder().setContent("Test button executed!"));
+          })
+      ]);
 
-  it("Command Interaction Returns Handler Message", async () => {
-    const response = await app.handleInteraction(JSON.stringify(testCommandInteraction), false);
+      app.components.has("testButton").should.be.true;
+      app.components.get("testButton")?.should.be.instanceOf(ButtonBuilder);
 
-    response.should.not.throw;
-    response.should.have.property("type", InteractionResponseType.ChannelMessageWithSource);
+      await app.commands.load([
+        new SlashCommandBuilder()
+          .setName("test2")
+          .setDescription("Test command")
+          .setHandler(async (context: SlashCommandContext) => {
+            await context.reply(
+              new MessageBuilder()
+                .setContent("Test command executed!")
+                .addActionRow(
+                  new ActionRowBuilder().addComponents(context.manager.components.createInstance("testButton"))
+                )
+            );
+          })
+      ]);
+
+      app.commands.has("test2").should.be.true;
+      app.commands.get("test2")?.should.be.instanceOf(LoadedSlashCommand);
+
+      const response = await app.handleInteraction(JSON.stringify(test2CommandInteraction), false);
+
+      response.should.deep.equal({
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          content: "Test command executed!",
+          components: [
+            {
+              type: ComponentType.ActionRow,
+              components: [
+                {
+                  type: ComponentType.Button,
+                  custom_id: "testButton|",
+                  label: "Test Button",
+                  style: 1
+                }
+              ]
+            }
+          ]
+        }
+      });
+    });
   });
 });
